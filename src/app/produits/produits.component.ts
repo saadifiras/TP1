2253a93f7dd313710bfd7172ec8809188691bd9a
saadifiras <isetnabeul81@gmail.com>
2023-10-03 22:24:27 +0200

    c mon travail

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html'
+  
+})
+export class ProduitsComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  
+}
